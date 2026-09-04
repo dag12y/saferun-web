b47198b03aba54a15a089e633f20621ab8f8ca7a
dagm <dagimyibabe19@gmail.com>
2026-09-04 16:18:23 +0300

    Add installation scripts with version handling and content type headers

--- public/install.sh
+++ public/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -eu
 
+VERSION=${SAFERUN_VERSION:-v1.0.4}
+export SAFERUN_VERSION="$VERSION"
+
 curl --fail --silent --show-error --location --proto '=https' --tlsv1.2 \
   https://raw.githubusercontent.com/dag12y/saferun/0ba0ddf6d4b809ab61195f833c507025263003a5/install.sh | sh
